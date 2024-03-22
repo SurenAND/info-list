1ff8266ebee0b9f2a27cb855a56991e5f8cf2364
AshkanZj <ashkanzojaji19@gmail.com>
2024-03-22 10:50:42 +0330

    create main variables

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 "use strict";
+
+// Main variables
+const informationSection = document.getElementById("information--section");
+const form = document.getElementById("info--form");
+const modalSection = document.getElementById("modal--section");
+let formTitleInput = document.getElementById("title");
+let formDescriptionInput = document.getElementById("description");
+let formDetailsInput = document.getElementById("details");
+let inputList = [];
+let currentId = 0;
