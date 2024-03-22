ad9de270783c8f776788c5b0869ff5a3d450f29e
AshkanZj <ashkanzojaji19@gmail.com>
2024-03-22 10:51:27 +0330

    create Validation function

--- script.js
+++ script.js
@@ -9,3 +9,33 @@ let formDescriptionInput = document.getElementById("description");
 let formDetailsInput = document.getElementById("details");
 let inputList = [];
 let currentId = 0;
+
+// Validation
+// Error messages
+const titleError = document.getElementById("title--error");
+const detailError = document.getElementById("details--error");
+// check if title and details input are empty
+function isValidForm() {
+  detailError.style.display = formDetailsInput.value ? "none" : "block";
+  titleError.style.display = formTitleInput.value ? "none" : "block";
+
+  // disappeared error if user type in title input
+  formTitleInput.addEventListener("input", (event) => {
+    event.preventDefault();
+    titleError.style.display = formTitleInput.value ? "none" : "block";
+  });
+
+  // disappeared error if user type details in input
+  formDetailsInput.addEventListener("input", (event) => {
+    event.preventDefault();
+    detailError.style.display = formDetailsInput.value ? "none" : "block";
+  });
+
+  return !formTitleInput.value && !formDetailsInput.value
+    ? false
+    : !formTitleInput.value
+    ? detailError.style.display
+    : !formDetailsInput.value
+    ? titleError.style.display
+    : true;
+}
