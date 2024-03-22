255aafd088c81edba2113b96e2657222113efedd
AshkanZj <ashkanzojaji19@gmail.com>
2024-03-22 10:53:26 +0330

    create get the submit date function

--- script.js
+++ script.js
@@ -68,3 +68,15 @@ function createInputObject(formValues, newDate, submitDate) {
     editDate: "",
   };
 }
+
+// get the submit date
+function formatDate(date) {
+  const year = date.getFullYear();
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+
+  return `${year}/${month}/${day} - ${hours}:${minutes}:${seconds}`;
+}
